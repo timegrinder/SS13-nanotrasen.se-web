4b3d5f6fe0c976b07019d856a608e6e7d0e43403
root <root@www.nsscyberiad.net>
2016-11-24 03:21:43 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nanotrasen</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+
+<div class="content">
+    <div>
+        <h1>Welcome to Nanotrasen</h1>
+    </div>
+    <div>
+        <div style="margin: 0; padding: 0;">
+            <h2 style="height: 10px">Server Links</h2>
+        </div>
+        <div class="servers">
+            <div style="font-size: 75%;">
+                (You need <a href="http://byond.com">BYOND</a> installed to use the below links)
+            </div>
+            <div>
+                <a href="byond://nanotrasen.se:6666">Paradise Station</a>
+            </div>
+        </div>
+    </div> <br>
+    <div class="server-content">
+        <div style="margin: 0; padding: 0;">
+            <h2 style="height: 10px">Paradise</h2>
+        </div>
+        <div>
+            <a href="http://nanotrasen.se/phpBB3">Paradise Forums</a>
+        </div>
+        <div>
+            <a href="http://nanotrasen.se/wiki">Paradise Wiki</a>
+        </div>
+        <div class="social">
+            <div style="margin: 0; padding: 0;">
+                <h3 style="height: 10px">Paradise Social</h3>
+            </div>
+            <div class="discord">
+                <a href="https://discord.gg/0oOrkebBwhVkGX0f">Join us on Discord</a>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div style="font-size: 150%">
+            <p>These servers are hosted in Florida. <br>
+            Use the respective forums if you need to reach our staff.</p>
+        </div>
+    </footer>
+</div>
+
+</body>
+</html>
