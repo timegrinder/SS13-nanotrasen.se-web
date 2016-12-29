540b564bc0b4f24674b057b70ac888f616c91792
Joris <Draconic-saint@users.noreply.github.com>
2016-12-29 19:04:55 +0100

    Complete redesign of the nanotrasen.se webpage.  (#2)
    * Add link to Guide for Beginniers
    
    Added a visible link to the Guide for Beginners page of the wiki.
    
    It might not be obvious to a new visitor to the site where to go or what to do at first, so a direct link to the guide will not only familiarize them with SS13 and Paradise, but it will also get them onto the wiki in the first place.
    
    This helps for users who may not initially deem it necessary to start by reading the wiki.
    
    * Complete redesign of the nanotrasen.se webpage. Still small things that need to be fixed.
    
    * fixed colors, set everything into rgba
    
    * fixed index page comments, just need to figure out what needs to go into the footer now.
    
    * fixed a tiny little code structure error, and finally added a licence to the footer.

--- index.php
+++ index.php
@@ -1,55 +1,61 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Nanotrasen</title>
-    <link rel="stylesheet" type="text/css" href="index.css">
+    <meta charset="UTF-8">
+    <title>Paradise Station</title>
+     <link rel="stylesheet" type="text/css" href="reset.css">
+    <link rel="stylesheet" type="text/css" href="nanotrasen.css">
+    <link href="https://fonts.googleapis.com/css?family=Exo" rel="stylesheet">
 </head>
 <body>
-
-<div class="content">
-    <div>
-        <h1>Welcome to Nanotrasen</h1>
-    </div>
-    <div>
-        <div style="margin: 0; padding: 0;">
-            <h2 style="height: 10px">Server Links</h2>
-        </div>
-        <div class="servers">
-            <div style="font-size: 75%;">
-                (You need <a href="http://byond.com">BYOND</a> installed to use the below links)
-            </div>
-            <div>
-                <a href="byond://nanotrasen.se:6666">Paradise Station</a>
-            </div>
-        </div>
-    </div> <br>
-    <div class="server-content">
-        <div style="margin: 0; padding: 0;">
-            <h2 style="height: 10px">Paradise</h2>
-        </div>
-        <div>
-            <a href="http://nanotrasen.se/phpBB3">Paradise Forums</a>
-        </div>
-        <div>
-            <a href="http://nanotrasen.se/wiki">Paradise Wiki</a>
-        </div>
-        <div class="social">
-            <div style="margin: 0; padding: 0;">
-                <h3 style="height: 10px">Paradise Social</h3>
-            </div>
-            <div class="discord">
-                <a href="https://discord.gg/0oOrkebBwhVkGX0f">Join us on Discord</a>
-            </div>
-        </div>
-    </div>
-
-    <footer>
-        <div style="font-size: 150%">
-            <p>These servers are hosted in Florida. <br>
-            Use the respective forums if you need to reach our staff.</p>
-        </div>
-    </footer>
-</div>
-
+    <div class="wrapper">
+    <nav>
+    
+       <ul>
+           <li id ="logo">  <a href="index.html"><img src="Images/Paradise2icon.PNG" alt="server logo"></a></li>
+           <li id="server"> <a href="byond://nanotrasen.se:6666">Server</a></li>
+           <li id="patreon"><a href="https://www.patreon.com/ParadiseStation">Patreon</a></li>
+           <li id="wiki">   <a href="https://nanotrasen.se/wiki/index.php/Main_Page">wiki</a></li>
+           <li id="forums"> <a href="https://nanotrasen.se/phpBB3/">forums</a></li>
+           <li id="discord"><a href="https://discord.gg/nuqD478">discord</a></li>
+           <li id="github"> <a href="https://github.com/ParadiseSS13/Paradise">github</a></li>
+       </ul>
+    </nav>
+    <main>
+        <h1>Paradise Station, a ss13 server</h1>
+        <div id="intro">
+            <p>
+                Paradise Station is a Space Station 13 server.
+                No idea what space station 13 is? Check out our new player guide <a href="https://nanotrasen.se/wiki/index.php/Guide_for_beginners"> here!</a>
+            </p>
+            <p>
+               We like to pride ourselves that the general playstyle on our server is a healthy mix of RP & Action. Come join us on the Cyberiad, and try to stay alive, at least for a while.
+            </p>
+       </div>
+        <ul>
+                <li>
+                   <h2>Forums & discord</h2>
+                   <p>Throughout the years, we've built a solid community around our server, with an active forum and a public discord server.</p>
+                   <p>The <a href="https://nanotrasen.se/phpBB3/">forums</a> are our official discussion space, where everything can be document easily.</p> 
+                   <p>The <a href="https://discord.gg/nuqD478">discord</a> is a more informal space where players and admins can hang out.</p>
+                </li>
+                <li>
+                   <h2>Patreon</h2>
+                   <p>Sadly, running a server is not free. Previously we accepted donations to keep that server running. Now we've set up a Patreon for that purpose. It also allows us to provide various cosmetic bonuses as a way of thanking our patrons.</p><p>You can find out more about the patron tiers on our <a href="https://www.patreon.com/ParadiseStation">Patreon</a> page!</p>
+                </li>
+                <li>
+                   <h2>Wiki</h2>
+                   <p>Space Station 13 isn't an easy game. It's got many complex system to play around with, and this may be daunting for new players. Many veteran players will agree though, that learning this game is just as fun as playing. </p><p> So don't be discouraged from trying out the game, just head over to the <a href="https://nanotrasen.se/wiki/index.php/Main_Page">wiki</a> and learn as you play. </p>
+                </li>
+                <li>
+                    <h2>Github</h2>
+                    <p>Space station 13 is an open source game.This means anyone can contribute. Most big servers run their own variant of the game, with distinct and unique elements to their codebase. We too, have our own version of the game, made possible through the hard work of our volunteer coders.</p> <p>You can view and contribute to our codebase <a href="https://github.com/ParadiseSS13/Paradise">at our github page.</a></p>
+                </li>
+            </ul>
+       
+   </main>
+   </div>
+   <footer><p>This webpage is licensed under the MIT License (MIT).</p></footer>
+   
 </body>
 </html>
